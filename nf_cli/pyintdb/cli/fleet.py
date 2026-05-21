7972f487758e448b94212e30998fd45a7f0428de
IntSPstudio <official.intsp@gmail.com>
2026-05-21 12:27:35 +0300

    R1221

--- nf_cli/pyintdb/cli/fleet.py
+++ nf_cli/pyintdb/cli/fleet.py
@@ -13,14 +13,22 @@ def fleet_group():
     """- Fleet management commands"""
     pass
 
-#REFUELING
-@fleet_group.command(name="refuel")
-@click.argument('vehicle_id')
-def add_refuel_event(vehicle_id):
-    """- Refuel a vehicle"""
-    click.echo(f"Refueling vehicle {vehicle_id}...")
+#ADDER
+@fleet_group.command(name="add")
+@click.argument('event', type=str)
+@click.argument('target', type=str)
+def default_add_event(event, target):
+    """- Options: Refuel, service"""
+    #REFUEL
+    if event == "refuel":
+        click.echo(f"Refueling vehicle id: {target}")
+    #SERVICE
+    if event == "service":
+        click.echo(f"Added service event to vehicle id: {target}")
+    else:
+        click.echo("Invalid event")
 
-#SCHEDULE EVENTS
+#SCHEDULE EVENTS TO FUTURE
 @fleet_group.command(name="schedule")
 @click.argument('vehicle_id', type=str)
 @click.argument('event_type', type=str)
